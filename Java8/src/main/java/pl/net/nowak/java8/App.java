453163e0b9d8a4bcbbd72cb98335cd2ed494a3c2
mno <mno@simple.com.pl>
2014-09-29 13:43:55 +0200

    init commit

---
+++ Java8/src/main/java/pl/net/nowak/java8/App.java
@@ -0,0 +1,13 @@
+package pl.net.nowak.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
