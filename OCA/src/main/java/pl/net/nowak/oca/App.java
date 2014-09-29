9b206e43d81551bc1d48db14e85068142ba775b1
mno <mno@simple.com.pl>
2014-09-29 13:50:02 +0200

    dodanie proj oca

---
+++ OCA/src/main/java/pl/net/nowak/oca/App.java
@@ -0,0 +1,13 @@
+package pl.net.nowak.oca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
